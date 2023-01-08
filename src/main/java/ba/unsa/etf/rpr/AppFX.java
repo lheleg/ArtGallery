db03371551856855aa1d967e8ea1a8cffbb46f98
lheleg <lheleg1@etf.unsa.ba>
2023-01-08 14:01:28 +0100

    AppFX class added

---
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -0,0 +1,24 @@
+package ba.unsa.etf.rpr;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+
+
+public class AppFX extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
