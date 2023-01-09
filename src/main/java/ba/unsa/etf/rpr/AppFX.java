3df5c68698aa26e6776c989880238e2808ebd366
lheleg <lheleg1@etf.unsa.ba>
2023-01-09 12:29:14 +0100

    small changes in AppFX

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.controllers.HomeController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -15,6 +16,8 @@ public class AppFX extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/home.fxml"));
+        HomeController homeController = new HomeController();
+        fxmlLoader.setController(homeController);
         Parent root = fxmlLoader.load();
         stage.setTitle("Art Gallery");
         stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
