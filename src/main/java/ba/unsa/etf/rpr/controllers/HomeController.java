c281fe7c9409561d45af32305e70893bc95f851a
lheleg <lheleg1@etf.unsa.ba>
2023-01-09 12:30:30 +0100

    HomeController class added

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -0,0 +1,25 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+
+public class HomeController {
+    public Button btnGall;
+
+    @FXML
+    void initialize() {
+
+    }
+
+    public void ActionEvent(ActionEvent actionEvent){
+
+    }
+}
