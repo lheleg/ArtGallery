aaf1df13d7bfe5eb703e1c0282cfe2cf5ced6239
lheleg <lheleg1@etf.unsa.ba>
2023-08-31 23:43:01 +0200

    class UserDaoSQLImpl small changes

--- src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
@@ -1,9 +1,11 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.Artist;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.GalleryException;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Map;
 
 /**
@@ -19,7 +21,17 @@ public class UserDaoSQLImpl extends AbstractDao<User> implements UserDao {
 
     @Override
     public User row2object(ResultSet rs) throws GalleryException {
-        return null;
+        try {
+            User user = new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setFirstName(rs.getString("firstName"));
+            user.setLastName(rs.getString("lastName"));
+            return user;
+        } catch (SQLException e) {
+            throw new GalleryException(e.getMessage(), e);
+        }
     }
 
     @Override
