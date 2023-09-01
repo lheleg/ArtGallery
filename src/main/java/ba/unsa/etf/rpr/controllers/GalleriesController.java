9aea125eaa3eb66623009ef0662d85f4d620a2d9
lheleg <lheleg1@etf.unsa.ba>
2023-09-01 23:54:25 +0200

    GalleriesController small changes

--- src/main/java/ba/unsa/etf/rpr/controllers/GalleriesController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/GalleriesController.java
@@ -34,7 +34,7 @@ public class GalleriesController {
         galPane.setVgap(5);
         galPane.setPadding(new Insets(7));
         galPane.setAlignment(Pos.CENTER);
-        System.out.println(Arrays.toString(galleries.toArray()));
+       // System.out.println(Arrays.toString(galleries.toArray()));
         for (Gallery gal : galleries) {
             ImageView imageView = new ImageView(new Image("https://www.blumandpoe.com/uploads/22600226/1687468251302/Kirken_Alexander_Tovborg_KHC_001_Photo_by_David_Stjernholm_thumbnail-1000-0x0x3500x2333_q85.jpg"));
             imageView.setFitWidth(275);
@@ -54,7 +54,6 @@ public class GalleriesController {
             stackPane.getChildren().addAll(imageView, rect, galName);
             stackPane.setAlignment(Pos.CENTER);
 
-//            stackPane.setOnMouseClicked(event -> openRoomListForHotel(hotel.getName()));
             galPane.add(stackPane, column, row);
             column++;
             if (column == 3) {
@@ -62,7 +61,6 @@ public class GalleriesController {
                 row++;
             }
         }
-        System.out.println("izaslo");
         scroller.setContent(galPane);
     }
 
