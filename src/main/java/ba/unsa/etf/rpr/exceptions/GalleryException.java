09d2ab9b43bf801191d5092f7fc155c157c9a954
lheleg <lheleg1@etf.unsa.ba>
2023-09-02 13:36:16 +0200

    class GalleryException commented

--- src/main/java/ba/unsa/etf/rpr/exceptions/GalleryException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/GalleryException.java
@@ -1,11 +1,25 @@
 package ba.unsa.etf.rpr.exceptions;
 
+/**
+ * The type Gallery exception.
+ */
 public class GalleryException extends Exception{
 
+    /**
+     * Instantiates a new Gallery exception
+     *
+     * @param message the message
+     * @param reason the reason
+     */
     public GalleryException(String message, Exception reason){
         super(message, reason);
     }
 
+    /**
+     * Instantiates a new Gallery exception
+     *
+     * @param message the message
+     */
     public GalleryException(String message){
         super(message);
     }
