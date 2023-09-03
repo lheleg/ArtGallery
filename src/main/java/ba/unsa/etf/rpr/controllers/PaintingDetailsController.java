000a34eb44d5af59ef8b206924f4cd6c6362e1e4
lheleg <lheleg1@etf.unsa.ba>
2023-09-03 16:05:09 +0200

    closeButton implemented for paintingDetails.fxml

--- src/main/java/ba/unsa/etf/rpr/controllers/PaintingDetailsController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/PaintingDetailsController.java
@@ -1,4 +1,12 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+
 public class PaintingDetailsController {
+    @FXML
+    private void closeAction(MouseEvent event) {
+        ((Node)(event.getSource())).getScene().getWindow().hide();
+    }
 }
