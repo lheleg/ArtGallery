88438235727c09be2b6abaf6765b97b071bb2fa9
lheleg <lheleg1@etf.unsa.ba>
2023-09-04 14:30:05 +0200

    LogInController initial implementation

--- src/main/java/ba/unsa/etf/rpr/controllers/LogInController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LogInController.java
@@ -1,4 +1,37 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LogInController {
+    /**
+     * The Username field
+     */
+    public TextField usernameField;
+    /**
+     * The Password field
+     */
+    public PasswordField passwordField;
+    /**
+     * The Login button
+     */
+    public Button logInButton;
+    /**
+     * The Wrong username in
+     */
+    @FXML
+    public Label wrongUsername;
+    /**
+     * The Wrong password in
+     */
+    @FXML
+    public Label wrongPassword;
+
+    @FXML
+    public void initialize() {
+    }
+
 }
