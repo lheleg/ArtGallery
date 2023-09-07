627612d9c661e4f9af3a6c98e5adc1103b99b0ff
lheleg <lheleg1@etf.unsa.ba>
2023-09-07 22:42:53 +0200

    RegisterController initial impl

--- src/main/java/ba/unsa/etf/rpr/controllers/RegisterController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegisterController.java
@@ -1,6 +1,8 @@
 package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.business.UserManager;
+import ba.unsa.etf.rpr.dao.AbstractDao;
+import ba.unsa.etf.rpr.domain.User;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -9,8 +11,10 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
+import java.io.FileReader;
+import java.sql.Connection;
+
 public class RegisterController {
-    private final UserManager u = new UserManager();
 
     /**
      * The First name field
@@ -78,12 +82,74 @@ public class RegisterController {
 
     private Stage primaryStage;
     public void initialize() {
+        registerButton.setOnAction(actionEvent -> {
+            String username = usernameField.getText().trim();
+            String password = passwordField.getText().trim();
+            String firstName = firstNameField.getText().trim();
+            String lastName = lastNameField.getText().trim();
+
+            boolean textFieldsFilled = true;
+
+            if (username.isEmpty()) {
+                wrongUsernameLabel.setText("This field cannot be empty.");
+                textFieldsFilled = false;
+            }
+
+            if (password.isEmpty()) {
+                wrongPasswordLabel.setText("This field cannot be empty");
+                textFieldsFilled = false;
+            }
+
+            if (firstName.isEmpty()) {
+                wrongFirstNameLabel.setText("This field cannot be empty");
+                textFieldsFilled = false;
+            }
+
+            if (lastName.isEmpty()) {
+                wrongLastNameLabel.setText("This field cannot be empty");
+                textFieldsFilled = false;
+            }
+
+            if (textFieldsFilled) {
+
+                User user = new User();
+                user.setFirstName(firstName);
+                user.setLastName(lastName);
+                user.setUsername(username);
+                user.setPassword(password);
+
+                try {
+                    Connection connection = AbstractDao.getConnection();
+
+                    UserManager u = new UserManager();
+                    User newUser = u.add(user);
+
+                    // Check if the user object was returned by the add User method from UserDaoSQLImpl.java
+                    if (newUser != null) {
+                        errorLabel.setText("User registered successfully. Log in to proceed.");
+                    } else {
+                        errorLabel.setText("Error registering user. Please try again.");
+                    }
+
+
+                    connection.close();
+
+                } catch (Exception exc) {
+                    exc.printStackTrace();
+                }
+            }
+        });
+
         firstNameField.textProperty().addListener((observable, oldValue, newValue) -> clearErrorMessage(wrongFirstNameLabel));
         lastNameField.textProperty().addListener((observable, oldValue, newValue) -> clearErrorMessage(wrongLastNameLabel));
         passwordField.textProperty().addListener((observable, oldValue, newValue) -> clearErrorMessage(wrongPasswordLabel));
         usernameField.textProperty().addListener((observable, oldValue, newValue) -> clearErrorMessage(wrongUsernameLabel));
     }
 
+    @FXML
+    private void registerButtonPerformed() {
+
+    }
     private void clearErrorMessage(Label label) {
         label.setText("");
     }
