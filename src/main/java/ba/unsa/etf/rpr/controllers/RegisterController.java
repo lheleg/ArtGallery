035af9d51cbee1baf952b53a3278517f508f884c
lheleg <lheleg1@etf.unsa.ba>
2023-09-07 20:22:08 +0200

    RegisterController changes

--- src/main/java/ba/unsa/etf/rpr/controllers/RegisterController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegisterController.java
@@ -1,4 +1,83 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.UserManager;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
 public class RegisterController {
+    private final UserManager u = new UserManager();
+
+    /**
+     * The First name field
+     */
+    public TextField firstNameField;
+
+    /**
+     * The Last name field
+     */
+    public TextField lastNameField;
+
+    /**
+     * The Username field
+     */
+    public TextField usernameField;
+
+    /**
+     * The Password field
+     */
+    public PasswordField passwordField;
+
+    /**
+     * The Login button
+     */
+    public Button registerButton;
+
+    /**
+     * The Error message Label
+     */
+    public Label errorLabel;
+
+    /**
+     * The Wrong username label,
+     * for error message
+     */
+    @FXML
+    public Label wrongUsernameLabel;
+
+    /**
+     * The Wrong password label,
+     * for error message
+     */
+    @FXML
+    public Label wrongPasswordLabel;
+
+    /**
+     * The Wrong First name label,
+     * for error message
+     */
+    @FXML
+    public Label wrongFirstNameLabel;
+
+    /**
+     * The Wrong Last name label,
+     * for error message
+     */
+    @FXML
+    public Label wrongLastNameLabel;
+
+    /**
+     * The Cancel image view
+     */
+    @FXML
+    public ImageView cancelImageView;
+
+    private Stage primaryStage;
+
+    public void initialize() {
+    }
 }
