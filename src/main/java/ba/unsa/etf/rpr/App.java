fbb0fd8a408f24a7ca792ac750cd1835049145f8
lheleg <lheleg1@etf.unsa.ba>
2023-09-08 15:49:33 +0200

    CLI initial impl

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,10 +1,10 @@
 package ba.unsa.etf.rpr;
 
-import ba.unsa.etf.rpr.dao.*;
 import ba.unsa.etf.rpr.domain.Artist;
 import ba.unsa.etf.rpr.domain.Gallery;
 import ba.unsa.etf.rpr.domain.Painting;
 
+import org.apache.commons.cli.Option;
 
 import java.util.List;
 
@@ -14,6 +14,32 @@ import java.util.List;
  */
 public class App 
 {
+    private static final Option addGallery = new Option("ag", "add-gallery", false, "Adding new gallery to db");
+    private static final Option addArtist = new Option("aa", "add-artist", false, "Adding new artist to db");
+    private static final Option addPainting = new Option("ap", "add-painting", false, "Adding new painting to db");
+    private static final Option deleteGallery = new Option("dg", "delete-gallery", false, "Deleting a gallery from db");
+    private static final Option deleteArtist = new Option("da", "delete-artist", false, "Deleting a artist from db");
+    private static final Option deletePainting = new Option("dp", "delete-painting", false, "Deleting a painting db");
+    private static final Option updateGallery = new Option("ug", "update-gallery", false, "Updating a gallery in db");
+    private static final Option updateArtist = new Option("ua", "update-artist", false, "Updating a artist in db");
+    private static final Option updatePainting = new Option("up", "update-painting", false, "Updating a painting in db");
+    private static final Option getGalleries = new Option("getG", "get-galleries", false, "Printing all galleries from db");
+    private static final Option getArtists = new Option("getA", "get-artists", false, "Printing all artists from db");
+    private static final Option getPaintings = new Option("getP", "get-paintings", false, "Printing all paintings from db");
+
+    private static final Option galleryName  = new Option("", "gallery-name", false, "");
+    private static final Option galleryUrl  = new Option("", "gallery-url", false, "");
+    private static final Option galleryImage  = new Option("", "gallery-image", false, "");
+    private static final Option artistFirstName  = new Option("", "artist-name", false, "");
+    private static final Option artistLastName  = new Option("", "artist-surname", false, "");
+    private static final Option artistStyle  = new Option("", "artist-style", false, "");
+    private static final Option artistImage  = new Option("", "artist-image", false, "");
+    private static final Option paintingAvailable  = new Option("", "painting-available", false, "");
+    private static final Option paintingTitle  = new Option("", "painting-title", false, "");
+    private static final Option paintingArtistId  = new Option("", "painting-artist", false, "");
+    private static final Option paintingGalleryId  = new Option("", "painting-gallery", false, "");
+    private static final Option paintingImage  = new Option("", "painting-image", false, "");
+
     public static void main( String[] args ) {
 
     }
