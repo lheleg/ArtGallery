de529fc415955fb53275109433b6b75e6759af4b
lheleg <lheleg1@etf.unsa.ba>
2023-09-09 23:11:29 +0200

    javadoc comments added

--- src/main/java/ba/unsa/etf/rpr/util/SecondaryStage.java
+++ src/main/java/ba/unsa/etf/rpr/util/SecondaryStage.java
@@ -4,14 +4,29 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+/**
+ * A custom secondary stage for JavaFX applications with specific styling and properties.
+ * This class is implemented as a singleton, ensuring that only one instance exists throughout the application.
+ */
 public class SecondaryStage extends Stage {
     private static SecondaryStage instance;
 
+    /**
+     * Private constructor to create a new instance of SecondaryStage.
+     * Configures the stage style and sets the application icon.
+     */
     private SecondaryStage() {
         this.initStyle(StageStyle.TRANSPARENT);
         this.getIcons().add(new Image("images/ikonica.png"));
     }
 
+    /**
+     * Get the singleton instance of SecondaryStage.
+     * If an instance does not exist, a new one is created. If it exists, it is closed.
+     * The stage is centered on the screen.
+     *
+     * @return the Singleton instance of SecondaryStage.
+     */
     public static SecondaryStage getInstance() {
         if (instance == null) {
             instance = new SecondaryStage();
